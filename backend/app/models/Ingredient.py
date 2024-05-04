ad4244163ae49fd9bf7989430848f8446a800732
elifdoylan <78874805+elifdoylan@users.noreply.github.com>
2024-05-04 20:45:43 +0300

    Update Ingredient.py

--- backend/app/models/Ingredient.py
+++ backend/app/models/Ingredient.py
@@ -8,10 +8,12 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     isHarmful = db.Column(db.Boolean, nullable=False)
+    harmfulSkin = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, name, isHarmful):
+    def __init__(self, name, isHarmful, harmfulSkin):
         self.name = name
         self.isHarmful = isHarmful
+        self.harmfulSkin = harmfulSkin
 
     def json(self):
-        return {"id": self.id, "name": self.name, "isHarmful": self.isHarmful}
+        return {"id": self.id, "name": self.name, "isHarmful": self.isHarmful, "harmfulSkin": self.harmfulSkin}
