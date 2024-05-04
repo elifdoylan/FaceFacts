d94ca92a1b9902dfdf24d4586eb23c5217bf777a
selcancelikel <95944187+selcancelikel@users.noreply.github.com>
2024-05-04 20:28:01 +0300

    Update ingredient_controller.py

--- backend/app/controllers/ingredient_controller.py
+++ backend/app/controllers/ingredient_controller.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 
 from .. import db  
-from ..models import Ingredient
+# from ..models import Ingredient
+from ..models import Blog
 
-ingredient_blueprint = Blueprint("ingredient", __name__)
+ingredient_blueprint = Blueprint("ingredient_blueprint", __name__)
 
 
-@ingredient_blueprint.route("/ingredient", methods=["POST"])
+
+
+@ingredient_blueprint.route("/ingredient", methods=["GET"])
 def create_ingredient():
     try:
         data = request.get_json()
@@ -20,13 +23,4 @@ def create_ingredient():
         return jsonify(new_ingredient.json()), 201
     except:
         return jsonify({"message": "An error occurred creating the ingredient"}), 500
-    
-
-
-@ingredient_blueprint.route("/ingredient", methods=["GET"])
-def get_ingredients():
-    try:
-        ingredients = Ingredient.query.all()
-        return jsonify([ingredient.json() for ingredient in ingredients]), 200
-    except:
-        return jsonify({"message": "An error occurred retrieving the ingredients"}), 500
+            
